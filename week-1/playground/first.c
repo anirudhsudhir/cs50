0234f301d395069c2c82643a59bb60b62e3c840e
Anirudh Sudhir <137381184+anirudhsudhir@users.noreply.github.com>
2023-11-04 21:43:33 +0530

    add makefile

--- week-1/playground/first.c
+++ week-1/playground/first.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 int main(void) {
+  int dummy = get_int("Enter a number: ");
   int x = 1;
   int y = 3;
   float z = (float)x / y;
-  printf("%f\n", z);
+  printf("%f%d\n", z, dummy);
 }
